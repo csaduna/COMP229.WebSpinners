7394af43b0292fc6cd5dd85b5338594d3ec776c3
csaduna <113253052+csaduna@users.noreply.github.com>
2022-11-12 17:01:46 -0500

    Views details added

---
+++ public/javascripts/appclient.js
@@ -0,0 +1,14 @@
+if(getTitle == "Ads")
+{
+    let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+    for(button of deleteButtons)
+    {
+        button.addEventListener('click', (event)=>{
+            if(!confirm("Are you sure?")) 
+            {
+                event.preventDefault();
+            }
+        });
+    }
+}
