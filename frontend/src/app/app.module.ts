86ca017029ae78b04c8e4e34a1a6cf5673015c55
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-12-07 14:33:24 -0500

    Updated app.module.ts
    Added RouterModule components so Abdul can continue with the next steps.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { IndexModule } from './products/index.module';
+import { IndexComponent } from './products/index.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,16 @@ import { IndexModule } from './products/index.module';
   ],
   imports: [
     BrowserModule,
-    IndexModule
+    IndexModule,
+    RouterModule.forRoot([
+      { path: "", component: IndexComponent },
+      // { path: "inventory/list", component: ListComponent },
+      // { path: "inventory/:mode", component: AddEditComponent, canActivate: [AuthGuard]},
+      // { path: "inventory/:mode/:id", component: AddEditComponent, canActivate: [AuthGuard]},
+      // { path: "users/signin", component: SignInComponent },
+      // { path: "users/signup", component: SignUpComponent },
+      { path:"**", redirectTo: "" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
